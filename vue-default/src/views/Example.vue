35cfbf19591eda9f2d8488c224096b23eded92f0
yunk_o <yunk_o@naver.com>
2021-09-07 00:22:08 +0900

    데이터바인딩

--- vue-default/src/views/Example.vue
+++ vue-default/src/views/Example.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
-        <input type="text" v-model="valueModel"/>
-        <p>{{valueModel}}</p>
-        <input type="number" v-model.number="valueModel2"> <br>
-        <textarea v-model="valueModel3">gg</textarea>
-        <div>{{valueModel3}}</div>
+        <!-- <select v-model="city">
+            <option value="02">서울</option>
+            <option value="21">부산</option>
+            <option value="064">제주</option>
+        </select> -->
+        <!-- <label><input type="checkbox" value="서울" v-model="checked">서울</label>
+        <label><input type="checkbox" value="부산" v-model="checked">부산</label>
+        <label><input type="checkbox" value="제주" v-model="checked">제주</label>
+        <br>
+        <span>체크한 지역 : {{ checked}}</span> -->
+        <label for=""><input type="radio" v-bind:value="radio1" v-model="picked">서을</label>
+        <label for=""><input type="radio" v-bind:value="radio2" v-model="picked">부산</label>
+        <label for=""><input type="radio" v-bind:value="radio3" v-model="picked">제주</label>
+        <br>
+        <span>선택한 지역 : {{picked}}</span>
     </div>
 </template>
 <script>
@@ -13,9 +23,12 @@ export default {
     components:{},
     data(){
         return{
-            valueModel:'South Korea',
-            valueModel2:12,
-            valueModel3:"안녕하세요"
+           city:"064",
+           checked:[],
+           picked:"",
+           radio1:"서울",
+           radio2:"부산",
+           radio3:"제주"
         };
     },
     setup() {},
