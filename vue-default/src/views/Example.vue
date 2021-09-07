33eb9fdad80dd0da7d3f3d8ce4de9291d1747eaf
yunk_o <yunk_o@naver.com>
2021-09-08 01:09:02 +0900

    속성 데이터 바인딩

--- vue-default/src/views/Example.vue
+++ vue-default/src/views/Example.vue
@@ -1,20 +1,8 @@
 <template>
     <div>
-        <!-- <select v-model="city">
-            <option value="02">서울</option>
-            <option value="21">부산</option>
-            <option value="064">제주</option>
-        </select> -->
-        <!-- <label><input type="checkbox" value="서울" v-model="checked">서울</label>
-        <label><input type="checkbox" value="부산" v-model="checked">부산</label>
-        <label><input type="checkbox" value="제주" v-model="checked">제주</label>
-        <br>
-        <span>체크한 지역 : {{ checked}}</span> -->
-        <label for=""><input type="radio" v-bind:value="radio1" v-model="picked">서을</label>
-        <label for=""><input type="radio" v-bind:value="radio2" v-model="picked">부산</label>
-        <label for=""><input type="radio" v-bind:value="radio3" v-model="picked">제주</label>
-        <br>
-        <span>선택한 지역 : {{picked}}</span>
+        <!-- <img v-bind:src="url" alt=""> -->
+        <input type="text" v-model="textValue">
+        <button type="button" v-bind:disabled="textValue==''">Click</button>
     </div>
 </template>
 <script>
@@ -23,12 +11,8 @@ export default {
     components:{},
     data(){
         return{
-           city:"064",
-           checked:[],
-           picked:"",
-           radio1:"서울",
-           radio2:"부산",
-           radio3:"제주"
+            url:"https://img.khan.co.kr/news/2006/12/07/6l07k11a.webp",
+            textValue:''
         };
     },
     setup() {},
