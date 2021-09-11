1495a5d8ecc9ea00322781516fa856a88ade55a1
yunk_o <yunk_o@naver.com>
2021-09-11 18:59:30 +0900

    v-if v-show

--- vue-default/src/views/Example.vue
+++ vue-default/src/views/Example.vue
@@ -1,34 +1,35 @@
 <template>
-    <!-- <div class="container" v-bind:class="{'active':isActive,'text-red':isRed}"> Class Binding</div> -->
-    <div v-bind:style="styleObject">인라인 스타일 Binding</div>
+    <div>
+        <!-- <h1 v-if="bRender">bRender값이 true이면, h1 블록이 화면에 보여지게 됩니다.</h1>
+        <h1 v-else>bRender값이 false이면, h1 블록이 화면에 보여지게 됩니다.</h1> -->
+
+        <!-- <h1 v-if="type=='A'">A</h1>
+        <h1 v-else-if="type=='B'">B</h1>
+        <h1 v-else-if="type=='C'">C</h1>
+        <h1 v-else>Oth er</h1> -->
+
+        <h1 v-show="bRender">A</h1>
+    </div>
 </template>
 <script>
 export default {
- 
+    name:'',
+    components:{},
     data(){
         return{
-            styleObject:{
-                backgroundColor:"yellow",
-                color:"red",
-                fontWeight:"bold"
-            }
-            // isActive:true,
-            // isRed:true,
+            // bRender:false
+
+            // type:"f"
+
+            bRender:true
         };
+    },
+    setup() {},
+    create() {},
+    mounted() {},
+    unmounted() {},
+    methods:{
+
     }
 }
 </script>
-<style>
-.container {
-    width:100%; 
-    height:100px;
-}
-.active {
-    background: yellow;
-    font-weight: bolder;
-}
-.text-red{
-    color:red
-}
-
-</style>
