8b4e553e53b835eace30196036c203e7546bc918
yunk-o <yunk_o@naver.com>
2021-09-17 00:18:26 +0900

    패키지 이용

--- vue-default/src/main.js
+++ vue-default/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  VueSweetalert2,
   render: h => h(App)
 }).$mount('#app')
 // createApp(App).use(router).mount('#app')
--- vue-default/src/views/Example.vue
+++ vue-default/src/views/Example.vue
@@ -1,9 +1,31 @@
 <template>
     <div>
-        <input type="text" v-model.number="x1">
-        <input type="text" v-model.number="x2">
-        <!-- <button type="button" @click="plusNumber">계산</button> -->
-        <input type="text" v-model="y" >
+        <div>
+            <select v-model="selectedCity">
+                <option :value="city.code" :key="i" v-for="(city,i) in cityList">{{city.name}}</option>
+            </select>
+            <input type="text" v-model="userName">
+            <!-- <button @click="searchUserList">조회</button> -->
+            <button @click="alert('조회')">조회</button>
+        </div>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>이름</th>
+                        <th>나이</th>
+                        <th>직업</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr :key="i" v-for="{person,i} in userList">
+                        <td>{{person.name}}</td>
+                        <td>{{person.age}}</td>
+                        <td>{{person.job}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 <script>
@@ -12,32 +34,42 @@ export default {
     components:{},
     data(){
         return{
-            x1:0,
-            x2:0,
-            // y:0
+            cityList:[
+                {name:'서울',code:'02'},
+                {name:'부신',code:'21'},
+                {name:'제주',code:'064'}
+            ],
+            selectedCity:'',
+            userName:'',
+            userList: []
         };
-    },
-    // watch:{
-    //     x1(){
-    //         this.y = this.x1 + this.x2;
-    //     },
-    //     x2(){
-    //         this.y = this.x1 + this.x2;
-    //     }
-    // },
-    computed: {
-        y(){
-            return this.x1 + this.x2;
-        }
     },
     setup() {},
     create() {},
     mounted() {},
     unmounted() {},
     methods:{
-       plusNumber(){
-           this.y = this.x1 + this.x2;
-       } 
+      searchUserList(){
+        //   const selectedCity = this.selectedCity;
+        //   const userName = this.userName 
+        const params = {
+            selectedCity : this.selectedCity,
+            userName : this.userName 
+        }
+
+        this.userList = this.getUserList(params);
+      },
+      getUserList(params){
+          console.log(params);
+
+          let list = [
+            {name:'유재석', age:50, job:"연예인"},
+            {name:'하하', age:40, job:"연예인"},
+            {name:'양세찬', age:37, job:"연예인"},
+            {name:'김종국', age:46, job:"연예인"},
+          ];
+          return list;
+      }  
     }
 }
 </script>
